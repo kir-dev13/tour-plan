2400926074b6fc28fa514c2af686424e089a1ffa
kir-dev13 <kirill87rabota@gmail.com>
2020-12-08 00:26:22 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,23 +1,45 @@
-var hotelSlider = new Swiper(".hotel-swiper-container", {
-  loop: true,
-  navigation: {
-    nextEl: ".hotel-slider-button--next",
-    prevEl: ".hotel-slider-button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-});
+$(document).ready(function () {
+  var hotelSlider = new Swiper(".hotel-swiper-container", {
+    loop: true,
+    navigation: {
+      nextEl: ".hotel-slider-button--next",
+      prevEl: ".hotel-slider-button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 
-var reviewSlider = new Swiper(".reviews-swiper-container", {
-  loop: true,
-  navigation: {
-    nextEl: ".reviews-slider__button--next",
-    prevEl: ".reviews-slider__button--prev",
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+  var reviewSlider = new Swiper(".reviews-swiper-container", {
+    loop: true,
+    navigation: {
+      nextEl: ".reviews-slider__button--next",
+      prevEl: ".reviews-slider__button--prev",
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
+
+  const modalButton = $("[data-toggle=modal]");
+
+  const closeButton = $(".modal__close");
+
+  let modalOverlay = $(".modal__overlay");
+  let modaldialog = $(".modal__dialog");
+
+  function openModal() {
+    modalOverlay.addClass("modal__overlay--visible");
+    modaldialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    modalOverlay.removeClass("modal__overlay--visible");
+    modaldialog.removeClass("modal__dialog--visible");
+  }
+
+  modalButton.on("click", openModal);
+  closeButton.on("click", closeModal);
 });
